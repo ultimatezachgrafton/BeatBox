d82834758148b7feeed47e95d9dcf21ec4ffce81
Zach Grafton <zachgrafton@gmail.com>
2019-10-26 13:31:08 -0700

    initial commit

--- app/src/main/java/learningcurve/zachg/beatbox/BeatBox.java
+++ app/src/main/java/learningcurve/zachg/beatbox/BeatBox.java
@@ -1,5 +1,30 @@
 package learningcurve.zachg.beatbox;
 
-public class BeatBox {
+import android.content.Context;
+import android.content.res.AssetManager;
+import android.util.Log;
 
+import java.io.IOException;
+
+public class BeatBox {
+    private static final String TAG = "BeatBox";
+    private static final String SOUNDS_FOLDER = "sample_sounds";
+
+    private AssetManager assets;
+
+    public BeatBox(Context context) {
+        assets = context.getAssets();
+        loadSounds();
+    }
+
+    private void loadSounds() {
+        String[] soundNames;
+        try {
+            soundNames = assets.list(SOUNDS_FOLDER);
+            Log.i(TAG, "Found " + soundNames.length + " sounds");
+        } catch (IOException ioe) {
+            Log.e(TAG, "Could not find list assets", ioe);
+            return;
+        }
+    }
 }
